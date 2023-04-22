7fbd7d7c1d298cb8941de3818d859dc278d718ab
notanoperative <psyopervert@gmail.com>
2023-04-22 16:17:05 -0300

    fix concurrence problems

--- main.py
+++ main.py
@@ -1,41 +1,59 @@
 from telethon import TelegramClient, events, sync
 import asyncio
 
-api_id = 222222222
-api_hash = 'aiaiaiai'
+api_id = "ele gosta"
+api_hash = ''
 
-client=TelegramClient('aaa', api_id,api_hash)
-
+client=TelegramClient('nu', api_id,api_hash)
 
 
 async def send_message_after_reply():
-    event = asyncio.Event()  # Cria um evento asyncio
+    event = asyncio.Event()  
+    fff =None
 
-    with open("output.txt", "a+") as file:
+    with open("g.txt", "a+") as file:
+        
+        
         async def handle_new_message(e):
             r_msg = e.message.message
+            
 
-            if "PARAM: XX" in r_msg:
-                file.write(r_msg)
-                print("registrado")
+            if "NÃO ENCONTRADO!" in r_msg:
+                
+                await client.send_message(' ', f"/  1 {fff}")
+
+                
+                print("reenviando")
+
             else:
-                print("Nao registrado")
+                print(f"registrado: {fff}")
+                file.write(r_msg)
             event.set()
+            
 
-        client.add_event_handler(handle_new_message, events.NewMessage(chats='Nomedochat'))
+        client.add_event_handler(handle_new_message, events.NewMessage(chats=' ', incoming=True))
 
         await client.start()
 
-        with open("input.txt", "r") as f:
+        with open("c.txt", "r") as f:
+            
             lines = f.readlines()
-            for line in lines:
-                fff = line.split("|")[0]
-                await client.send_message('Nomedochat', f"/telegramparam {fff}")
+            for i in range(len(lines)):
+                fff = lines[i].split("|")[0]
+                print(f"Na linha: {i}")
+                await client.send_message(' ', f"/  2 {fff}")
+              
                 try:
-                    await asyncio.wait_for(event.wait(), timeout=10.0)
+                    await asyncio.wait_for(event.wait(), timeout=30.0)
                 except asyncio.TimeoutError:
                     print("Timeout, reenviando mensagem...")
+                    await client.send_message(' ', "Rsrs")
+
+                    await client.send_message(' ', f"/  3 {fff}")
+
                     continue
+               
+
                 event.clear()  
 
 loop = asyncio.get_event_loop()
